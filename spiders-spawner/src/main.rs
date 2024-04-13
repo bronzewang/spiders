54fb79807e69fdba8222fffdbeb0448b0be36434
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-04-13 22:22:54 +0800

    clap

--- spiders-spawner/src/main.rs
+++ spiders-spawner/src/main.rs
@@ -1,3 +1,18 @@
+// use clap::Parser;
+
+use std::{path::PathBuf};
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+struct Cli {
+    #[arg(short='s', long="solid")]
+    solid_file: Option<PathBuf>,
+    #[arg(short='f', long="fluid")]
+    fluid_file: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("cli: {:?}", cli);
 }
