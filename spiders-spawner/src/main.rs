582013321ab8821f0e0745ab546033bf1c50c74e
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-04-14 13:55:57 +0800

    config json file parse

--- spiders-spawner/src/main.rs
+++ spiders-spawner/src/main.rs
@@ -1,9 +1,37 @@
 // use clap::Parser;
 
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::BufReader;
 use clap::Parser;
-#[allow(unused_imports)]
-use serde_json::Value;
+//use futures::io::BufReader;
+use serde::Deserialize;
+// #[allow(unused_imports)]
+// use serde_json::Value;
+
+#[derive(Deserialize, Debug)]
+pub struct RoleMaster {
+    pub binary: PathBuf,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Menage {
+    pub name: String,
+    pub step: u8,
+    pub master: RoleMaster,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SolidConfig {
+    pub surname: String,
+    pub version: String,
+    pub menages: Vec<Menage>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct FluidConfig {
+    pub menages: Vec<Menage>,
+}
 
 #[derive(clap::Parser, Debug)]
 struct Cli {
@@ -13,8 +41,27 @@ struct Cli {
     fluid_file: Option<PathBuf>,
 }
 
-fn main() {
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     println!("cli: {:?}", cli);
+
+    // let solid_file = if Same(solid) = cli.solid_file {
+    //     solid
+    // } else {
+    //     PathBuf::new()
+    // }
+
+    let file = File::open("./config/solid.json")?;
+    let reader = BufReader::new(file);
+    let solid: SolidConfig = serde_json::from_reader(reader)?;
+    println!("solid config {:?}", solid);
+
+    let file = File::open("./config/fluid.json")?;
+    let reader = BufReader::new(file);
+    let fluid: FluidConfig = serde_json::from_reader(reader)?;
+    println!("fluid config {:?}", fluid);
+
+    Ok(())
 }
