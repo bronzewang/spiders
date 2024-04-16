dfa4bf290b13ee59fa0eaf36eb5416981ed717a7
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-04-16 23:10:40 +0800

    udev

--- scenes/spiders-dossier/src/main.rs
+++ scenes/spiders-dossier/src/main.rs
@@ -1,5 +1,8 @@
 use std::path::{PathBuf};
 use clap::Parser;
+use futures_util::future::ready;
+use futures_util::stream::StreamExt;
+use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
 
 pub struct Canbus {
     pub baudrate: u32,
@@ -44,14 +47,37 @@ struct Cli {
     solid_xplug: Option<PathBuf>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let cli = Cli::parse();
 
     println!("cli: {:?}", cli);
 
-    for dev in nusb::list_devices().unwrap() {
-        println!("dev {:#?}", dev);
-    }
+    // for dev in nusb::list_devices().unwrap() {
+    //     println!("dev {:#?}", dev);
+    // }
+
+    let builder = MonitorBuilder::new()
+        .expect("Couldn't create builder")
+        .match_subsystem_devtype("usb", "usb_device")
+        .expect("Failed to add filter for USB devices");
+
+    let monitor: AsyncMonitorSocket = builder
+        .listen()
+        .expect("Couldn't create MonitorSocket")
+        .try_into()
+        .expect("Couldn't create AsyncMonitorSocket");
+    monitor.for_each(|event| {
+        if let Ok(event) = event {
+            println!(
+                "Hotplug event: {}: {}",
+                event.event_type(),
+                event.device().syspath().display()
+            );
+        }
+        ready(())
+    }).await;
+    println!("udev return");
 
     Ok(())
 }
