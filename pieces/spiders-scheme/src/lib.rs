60d1ac3473b8669b953aa425a45fbed2b61dceb4
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-05-24 00:05:46 +0800

    axum server

--- pieces/spiders-scheme/src/lib.rs
+++ pieces/spiders-scheme/src/lib.rs
@@ -1,2 +1,6 @@
 pub mod entity;
 pub mod scheme;
+
+pub const SPIDERS_WEB_PORT_BASE: u16 = 63000;
+pub const SPIDERS_GRPC_PORT_BASE: u16 = 22000;
+pub const SPIDERS_WEBSOCKET_PORT_BASE: u16 = 55000;
--- scenes/spiders-dossier/src/main.rs
+++ scenes/spiders-dossier/src/main.rs
@@ -4,6 +4,14 @@ use clap::Parser;
 // use futures_util::stream::StreamExt;
 use serde::Deserialize;
 // use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
+use axum::{response::Html, routing::get, Router};
+use log::{error, Level};
+use opentelemetry::KeyValue;
+use opentelemetry_appender_log::OpenTelemetryLogBridge;
+use opentelemetry_sdk::logs::{Config, LoggerProvider};
+use opentelemetry_sdk::Resource;
+
+use spiders_scheme::SPIDERS_WEB_PORT_BASE;
 
 pub mod greeter {
 	include!(concat!(env!("OUT_DIR"), "/greeter.rs"));
@@ -32,6 +40,7 @@ pub struct Innate {
     pub sxplug_larder: PathBuf,
     pub fxplug_larder: PathBuf,
 
+    pub id: u16,
     pub name: String,
 }
 
@@ -54,79 +63,112 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let innate: Innate = serde_json::from_reader(innate_reader)?;
     println!("innate {:?}", innate);
 
-    // let builder = MonitorBuilder::new()
-    //     .expect("Couldn't create builder")
-    //     .match_subsystem_devtype("usb", "usb_device")
-    //     .expect("Failed to add filter for USB devices");
+    let exporter = opentelemetry_stdout::LogExporterBuilder::default().build();
+    let logger_provider = LoggerProvider::builder()
+        .with_config(
+            Config::default().with_resource(Resource::new(vec![KeyValue::new(
+                "service.name",
+                "spiders-dossier",
+            )]))
+        )
+        .with_simple_exporter(exporter)
+        .build();
 
-    // let monitor: AsyncMonitorSocket = builder
-    //     .listen()
-    //     .expect("Couldn't create MonitorSocket")
-    //     .try_into()
-    //     .expect("Couldn't create AsyncMonitorSocket");
-    // monitor.for_each(|event| {
-    //     if let Ok(event) = event {
-    //         println!(
-    //             "Hotplug event: {}: {}",
-    //             event.event_type(),
-    //             event.device().syspath().display()
-    //         );
-    //     }
-    //     ready(())
-    // }).await;
-    // println!("udev return");
+    // Setup Log Appender for the log crate.
+    let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
+    log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
+    log::set_max_level(Level::Error.to_level_filter());
 
-    // let mut enumerator = udev::Enumerator::new()?;
-    // // enumerator.match_subsystem("usb")?;
-
-    // for device in enumerator.scan_devices()? {
-    //     // println!("{:#?}", device.syspath());
-    //     // println!("{:#?}", device.devpath());
-    //     println!();
-    //     println!("{:#?}", device);
-
-    //     println!("  [properties]");
-    //     for property in device.properties() {
-    //         println!("    - {:?} {:?}", property.name(), property.value());
-    //     }
-
-    //     println!("  [attributes]");
-    //     for attribute in device.attributes() {
-    //         println!("    - {:?} {:?}", attribute.name(), attribute.value());
-    //     }
-    // }
-
-    // tracing_subscriber::registry()
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env()
-    //             .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
-    //     )
-    //     .with(tracing_subscriber::fmt::layer())
-    //     .init();
-
-    // let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
-
-    // // build our application with some routes
-    // let app = Router::new()
-    //     .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
-    //     .route("/ws", get(ws_handler))
-    //     // logging so we can see whats going on
-    //     .layer(
-    //         TraceLayer::new_for_http()
-    //             .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-    //     );
-
-    // // run it with hyper
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    //     .await
-    //     .unwrap();
-    // tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    // axum::serve(
-    //     listener,
-    //     app.into_make_service_with_connect_info::<SocketAddr>(),
-    // )
-    // .await
-    // .unwrap();
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}",
+                                                         SPIDERS_WEB_PORT_BASE+innate.id))
+        .await
+        .unwrap();
+    // run it
+    error!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World! I am Spiders.</h1>")
+}
+
+// fn udev_parse()
+// {
+//     let builder = MonitorBuilder::new()
+//         .expect("Couldn't create builder")
+//         .match_subsystem_devtype("usb", "usb_device")
+//         .expect("Failed to add filter for USB devices");
+
+//     let monitor: AsyncMonitorSocket = builder
+//         .listen()
+//         .expect("Couldn't create MonitorSocket")
+//         .try_into()
+//         .expect("Couldn't create AsyncMonitorSocket");
+//     monitor.for_each(|event| {
+//         if let Ok(event) = event {
+//             println!(
+//                 "Hotplug event: {}: {}",
+//                 event.event_type(),
+//                 event.device().syspath().display()
+//             );
+//         }
+//         ready(())
+//     }).await;
+//     println!("udev return");
+
+//     let mut enumerator = udev::Enumerator::new()?;
+//     // enumerator.match_subsystem("usb")?;
+
+//     for device in enumerator.scan_devices()? {
+//         // println!("{:#?}", device.syspath());
+//         // println!("{:#?}", device.devpath());
+//         println!();
+//         println!("{:#?}", device);
+
+//         println!("  [properties]");
+//         for property in device.properties() {
+//             println!("    - {:?} {:?}", property.name(), property.value());
+//         }
+
+//         println!("  [attributes]");
+//         for attribute in device.attributes() {
+//             println!("    - {:?} {:?}", attribute.name(), attribute.value());
+//         }
+//     }
+
+//     tracing_subscriber::registry()
+//         .with(
+//             tracing_subscriber::EnvFilter::try_from_default_env()
+//                 .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
+//         )
+//         .with(tracing_subscriber::fmt::layer())
+//         .init();
+
+//     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+
+//     // build our application with some routes
+//     let app = Router::new()
+//         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
+//         .route("/ws", get(ws_handler))
+//         // logging so we can see whats going on
+//         .layer(
+//             TraceLayer::new_for_http()
+//                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+//         );
+
+//     // run it with hyper
+//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+//         .await
+//         .unwrap();
+//     tracing::debug!("listening on {}", listener.local_addr().unwrap());
+//     axum::serve(
+//         listener,
+//         app.into_make_service_with_connect_info::<SocketAddr>(),
+//     )
+//     .await
+//     .unwrap();
+// }
