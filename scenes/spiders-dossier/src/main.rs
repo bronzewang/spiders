38ccaeb90eba9fba33df5ce8ba76a21fb6c52189
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-06-05 00:04:22 +0800

    greeter proto

--- scenes/spiders-dossier/src/main.rs
+++ scenes/spiders-dossier/src/main.rs
@@ -192,7 +192,6 @@ use serialport::{available_ports, SerialPortType};
 #[allow(unused)]
 async fn serial_parse() -> Result<(), Box<dyn std::error::Error>>
 {
-    println!("1");
     match available_ports() {
         Ok(ports) => {
             println!("ports count {}", ports.len());
@@ -242,34 +241,36 @@ async fn serial_parse() -> Result<(), Box<dyn std::error::Error>>
     Ok(())
 }
 
-    // tracing_subscriber::registry()
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env()
-    //             .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
-    //     )
-    //     .with(tracing_subscriber::fmt::layer())
-    //     .init();
+// async fn websocket_init() {
+//     tracing_subscriber::registry()
+//         .with(
+//             tracing_subscriber::EnvFilter::try_from_default_env()
+//                 .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
+//         )
+//         .with(tracing_subscriber::fmt::layer())
+//         .init();
 
-    // let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+//     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
 
-    // // build our application with some routes
-    // let app = Router::new()
-    //     .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
-    //     .route("/ws", get(ws_handler))
-    //     // logging so we can see whats going on
-    //     .layer(
-    //         TraceLayer::new_for_http()
-    //             .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-    //     );
+//     // build our application with some routes
+//     let app = Router::new()
+//         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
+//         .route("/ws", get(ws_handler))
+//         // logging so we can see whats going on
+//         .layer(
+//             TraceLayer::new_for_http()
+//                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+//         );
 
-    // // run it with hyper
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    //     .await
-    //     .unwrap();
-    // tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    // axum::serve(
-    //     listener,
-    //     app.into_make_service_with_connect_info::<SocketAddr>(),
-    // )
-    // .await
-    // .unwrap();
+//     // run it with hyper
+//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+//         .await
+//         .unwrap();
+//     tracing::debug!("listening on {}", listener.local_addr().unwrap());
+//     axum::serve(
+//         listener,
+//         app.into_make_service_with_connect_info::<SocketAddr>(),
+//     )
+//     .await
+//     .unwrap();
+// }
