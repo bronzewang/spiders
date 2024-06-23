f946e43efa0626e4f2bba13e7baeaccb5337ed7b
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-06-23 11:39:15 +0800

    adpater to letponic

--- scenes/spiders-visitor/src/app.rs
+++ scenes/spiders-visitor/src/app.rs
@@ -1,5 +1,76 @@
-use leptos::leptos_dom::ev::SubmitEvent;
+// use leptos::leptos_dom::ev::SubmitEvent;
+// use leptos::*;
+// use serde::{Deserialize, Serialize};
+// use serde_wasm_bindgen::to_value;
+// use wasm_bindgen::prelude::*;
+
+// #[wasm_bindgen]
+// extern "C" {
+//     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+//     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+// }
+
+// #[derive(Serialize, Deserialize)]
+// struct GreetArgs<'a> {
+//     name: &'a str,
+// }
+
+// #[component]
+// pub fn App() -> impl IntoView {
+//     let (name, set_name) = create_signal(String::new());
+//     let (greet_msg, set_greet_msg) = create_signal(String::new());
+
+//     let update_name = move |ev| {
+//         let v = event_target_value(&ev);
+//         set_name.set(v);
+//     };
+
+//     let greet = move |ev: SubmitEvent| {
+//         ev.prevent_default();
+//         spawn_local(async move {
+//             let name = name.get_untracked();
+//             if name.is_empty() {
+//                 return;
+//             }
+
+//             let args = to_value(&GreetArgs { name: &name }).unwrap();
+//             // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//             let new_msg = invoke("greet", args).await.as_string().unwrap();
+//             set_greet_msg.set(new_msg);
+//         });
+//     };
+
+//     view! {
+//         <main class="container">
+//             <div class="row">
+//                 <a href="https://tauri.app" target="_blank">
+//                     <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
+//                 </a>
+//                 <a href="https://docs.rs/leptos/" target="_blank">
+//                     <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
+//                 </a>
+//             </div>
+
+//             <p>"Click on the Tauri and Leptos logos to learn more."</p>
+
+//             <form class="row" on:submit=greet>
+//                 <input
+//                     id="greet-input"
+//                     placeholder="Enter a name..."
+//                     on:input=update_name
+//                 />
+//                 <button type="submit">"Greet"</button>
+//             </form>
+
+//             <p><b>{ move || greet_msg.get() }</b></p>
+//         </main>
+//     }
+// }
+
+
+use leptonic::prelude::*;
 use leptos::*;
+use leptos_meta::{Meta, Title};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
@@ -17,16 +88,12 @@ struct GreetArgs<'a> {
 
 #[component]
 pub fn App() -> impl IntoView {
+    tracing::info!("Welcome to Leptonic");
+
     let (name, set_name) = create_signal(String::new());
     let (greet_msg, set_greet_msg) = create_signal(String::new());
 
-    let update_name = move |ev| {
-        let v = event_target_value(&ev);
-        set_name.set(v);
-    };
-
-    let greet = move |ev: SubmitEvent| {
-        ev.prevent_default();
+    let greet = move || {
         spawn_local(async move {
             let name = name.get_untracked();
             if name.is_empty() {
@@ -40,29 +107,37 @@ pub fn App() -> impl IntoView {
         });
     };
 
+    let (count, set_count) = create_signal(0);
+
     view! {
-        <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-                </a>
-            </div>
+        <Meta name="charset" content="UTF-8"/>
+        <Meta name="description" content="Leptonic Tauri template"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Meta name="theme-color" content="#e66956"/>
 
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
+        <Title text="Leptonic Tauri template"/>
 
-            <form class="row" on:submit=greet>
-                <input
-                    id="greet-input"
-                    placeholder="Enter a name..."
-                    on:input=update_name
-                />
-                <button type="submit">"Greet"</button>
-            </form>
+        <Root default_theme=LeptonicTheme::default()>
+            <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; min-height: 100%; min-width: 100%">
+                <H2>"Welcome to Leptonic"</H2>
 
-            <p><b>{ move || greet_msg.get() }</b></p>
-        </main>
+                <Stack spacing=Size::Em(2.0)>
+                    <div style="width: 100%;">
+                        <div style="margin-top: 3em;">"Count: " {move || count.get()}</div>
+                        <Button on_click=move|_| set_count.update(|c| *c += 1)>
+                            "Increase"
+                        </Button>
+                    </div>
+
+                    <div style="width: 100%;">
+                        <TextInput get=name set=set_name placeholder="Enter a name..."/>
+                        <Button on_click=move|_| greet()>
+                            "Greet"
+                        </Button>
+                        <p><b>{ move || greet_msg.get() }</b></p>
+                    </div>
+                </Stack>
+            </Box>
+        </Root>
     }
 }
--- scenes/spiders-visitor/src/main.rs
+++ scenes/spiders-visitor/src/main.rs
@@ -5,6 +5,11 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default_with_config(
+        tracing_wasm::WASMLayerConfigBuilder::default()
+            .set_max_level(tracing::Level::DEBUG)
+            .build(),
+    );
     mount_to_body(|| {
         view! {
             <App/>
