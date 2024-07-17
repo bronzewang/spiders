51c3df75ca50d2cbbbadf60a4afe2e2af0a7af84
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-07-17 23:44:51 +0800

    leptos

--- scenes/spiders-dossier/src/main.rs
+++ scenes/spiders-dossier/src/main.rs
@@ -64,6 +64,10 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    // use leptos::{logging::log, *};
+    // use leptos_axum::{generate_route_list, LeptosRoutes};
+    // use spiders_dossier::{app::*, fallback::file_and_error_handler};
+
     let cli = Cli::parse();
     println!("cli: {:?}", cli);
 
@@ -111,6 +115,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
             .with_description("The metrics HTTP response sizes in bytes.")
             .init(),
     });
+
+    // let conf = get_configuration(None).await.unwrap();
+    // let addr = conf.leptos_options.site_addr;
+    // let leptos_options = conf.leptos_options;
+    // // Generate the list of routes in your Leptos App
+    // let routes = generate_route_list(App);
+
+    // let app = Router::new()
+    //     .leptos_routes(&leptos_options, routes, || view! { <App/> })
+    //     .fallback(file_and_error_handler)
+    //     .with_state(leptos_options);
 
     // build our application with a route
     let app = Router::new().route("/", get(handler)).with_state(state);
