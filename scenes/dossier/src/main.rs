e8c19c4f0964d48c340c419bdfc85beecec3d685
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-07-21 22:53:22 +0800

    udev

--- scenes/dossier/src/main.rs
+++ scenes/dossier/src/main.rs
@@ -5,7 +5,6 @@ use axum::{response::Html, extract::State, routing::get, Router};
 use log::{error, Level};
 use opentelemetry_appender_log::OpenTelemetryLogBridge;
 use opentelemetry_sdk::{logs::{Config, LoggerProvider}, metrics::SdkMeterProvider};
-use opentelemetry_sdk::Resource;
 use prometheus::{Registry};
 use std::sync::Arc;
 use opentelemetry::{
@@ -13,8 +12,8 @@ use opentelemetry::{
     KeyValue,
 };
 
-// use spiders_scheme::SPIDERS_WEB_PORT_BASE;
-const SPIDERS_WEB_PORT_BASE: u16 = 62999;
+// // use spiders_scheme::SPIDERS_WEB_PORT_BASE;
+// const SPIDERS_WEB_PORT_BASE: u16 = 62999;
 
 // pub mod greeter {
 // 	include!(concat!(env!("OUT_DIR"), "/greeter.rs"));
@@ -55,12 +54,12 @@ struct Cli {
     sibase_innate: Option<PathBuf>,
 }
 
-// #[derive(Debug)]
-// struct AppState {
-//     registry: Registry,
-//     http_counter: Counter<u64>,
-//     http_body_gauge: Histogram<u64>,
-// }
+#[derive(Debug)]
+struct AppState {
+    registry: Registry,
+    http_counter: Counter<u64>,
+    http_body_gauge: Histogram<u64>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
@@ -77,45 +76,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let innate: Innate = serde_json::from_reader(innate_reader)?;
     println!("innate {:?}", innate);
 
-    // udev_parse().await?;
+    udev_parse().await?;
     serial_parse().await?;
 
-    // let exporter = opentelemetry_stdout::LogExporterBuilder::default().build();
-    // let logger_provider = LoggerProvider::builder()
-    //     .with_config(
-    //         Config::default().with_resource(Resource::new(vec![KeyValue::new(
-    //             "service.name",
-    //             "spiders-dossier",
-    //         )]))
-    //     )
-    //     .with_simple_exporter(exporter)
-    //     .build();
+    let exporter = opentelemetry_stdout::LogExporterBuilder::default().build();
+    let logger_provider = LoggerProvider::builder()
+        .with_config(
+            Config::default().with_resource(opentelemetry_sdk::Resource::new(vec![KeyValue::new(
+                "service.name",
+                "spiders-dossier",
+            )]))
+        )
+        .with_simple_exporter(exporter)
+        .build();
 
-    // // Setup Log Appender for the log crate.
-    // let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
-    // log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
-    // log::set_max_level(Level::Error.to_level_filter());
+    // Setup Log Appender for the log crate.
+    let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
+    log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
+    log::set_max_level(Level::Error.to_level_filter());
 
-    // let registry = Registry::new();
-    // let exporter = opentelemetry_prometheus::exporter()
-    //     .with_registry(registry.clone())
-    //     .build()?;
-    // let provider = SdkMeterProvider::builder().with_reader(exporter).build();
+    let registry = Registry::new();
+    let exporter = opentelemetry_prometheus::exporter()
+        .with_registry(registry.clone())
+        .build()?;
+    let provider = SdkMeterProvider::builder().with_reader(exporter).build();
 
-    // let meter = provider.meter("spiders-dossier");
-    // let state = Arc::new(AppState {
-    //     registry,
-    //     http_counter: meter
-    //         .u64_counter("http_requests_total")
-    //         .with_description("Total number of HTTP requests made.")
-    //         .init(),
-    //     http_body_gauge: meter
-    //         .u64_histogram("example.http_response_size")
-    //         .with_unit(Unit::new("By"))
-    //         .with_description("The metrics HTTP response sizes in bytes.")
-    //         .init(),
-    // });
-    // // build our application with a route
+    let meter = provider.meter("spiders-dossier");
+    let state = Arc::new(AppState {
+        registry,
+        http_counter: meter
+            .u64_counter("http_requests_total")
+            .with_description("Total number of HTTP requests made.")
+            .init(),
+        http_body_gauge: meter
+            .u64_histogram("example.http_response_size")
+            .with_unit(Unit::new("By"))
+            .with_description("The metrics HTTP response sizes in bytes.")
+            .init(),
+    });
+    // build our application with a route
     // let app = Router::new().route("/", get(handler)).with_state(state);
     // let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}",
     //                                                      SPIDERS_WEB_PORT_BASE+innate.id))
@@ -129,6 +128,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let routes = generate_route_list(App);
 
     let app = Router::new()
+        .route("/api", get(handler)).with_state(state)
         .leptos_routes(&leptos_options, routes, || view! { <App/> })
         .fallback(file_and_error_handler)
         .with_state(leptos_options);
@@ -141,13 +141,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     Ok(())
 }
 
-// async fn handler(State(state): State<Arc<AppState>>) -> Html<&'static str> {
-//     error!("{:?}", state);
-//     error!("{:?} {:?} {:?}", state.registry, state.http_counter, state.http_body_gauge);
+async fn handler(State(state): State<Arc<AppState>>) -> Html<&'static str> {
+    error!("{:?}", state);
+    error!("{:?} {:?} {:?}", state.registry, state.http_counter, state.http_body_gauge);
 
-//     error!("respond");
-//     Html("<h1>Hello, World! I am Spiders.</h1>")
-// }
+    error!("respond");
+    Html("<h1>Hello, World! I am Spiders.</h1>")
+}
 
 // use futures_util::future::ready;
 // use futures_util::stream::StreamExt;
