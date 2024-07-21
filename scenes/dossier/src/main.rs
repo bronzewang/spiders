352db8b6ffc57fb00aae437d23d938ab716ac835
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-07-21 23:29:33 +0800

    format

--- scenes/dossier/src/main.rs
+++ scenes/dossier/src/main.rs
@@ -63,7 +63,8 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    use leptos::{logging::log, *};
+    use leptos::*;
+    // use leptos::{logging::log};
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use spiders_dossier::{app::*, fallback::file_and_error_handler};
 
@@ -257,37 +258,3 @@ async fn serial_parse() -> Result<(), Box<dyn std::error::Error>>
 
     Ok(())
 }
-
-// async fn websocket_init() {
-//     tracing_subscriber::registry()
-//         .with(
-//             tracing_subscriber::EnvFilter::try_from_default_env()
-//                 .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
-//         )
-//         .with(tracing_subscriber::fmt::layer())
-//         .init();
-
-//     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
-
-//     // build our application with some routes
-//     let app = Router::new()
-//         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
-//         .route("/ws", get(ws_handler))
-//         // logging so we can see whats going on
-//         .layer(
-//             TraceLayer::new_for_http()
-//                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-//         );
-
-//     // run it with hyper
-//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-//         .await
-//         .unwrap();
-//     tracing::debug!("listening on {}", listener.local_addr().unwrap());
-//     axum::serve(
-//         listener,
-//         app.into_make_service_with_connect_info::<SocketAddr>(),
-//     )
-//     .await
-//     .unwrap();
-// }
