a5c5d50cd18b19a7bc48e3a6ef9a5104de157a4e
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-07-27 21:58:31 +0800

    innate

--- scenes/dossier/src/innate.rs
+++ scenes/dossier/src/innate.rs
@@ -1,1 +1,52 @@
 
+
+// 上电初始化一次的参数 'static
+#[derive(Deserialize, Debug)]
+pub struct Innate {
+	// 公共信息存放的位置，如数据库文件assets.db
+    pub fibase_shrine: PathBuf,
+    pub sibase_shrine: PathBuf,
+
+	// 参数存放的位置
+    pub fibase_valver: PathBuf,
+    pub sibase_valver: PathBuf,
+
+    pub fibase_snaper: PathBuf,
+    pub sibase_snaper: PathBuf,
+
+    pub sibase_larder: PathBuf,
+    pub fibase_larder: PathBuf,
+    pub sxmass_larder: PathBuf,
+    pub fxmass_larder: PathBuf,
+    pub sxplug_larder: PathBuf,
+    pub fxplug_larder: PathBuf,
+
+    pub id: u16,
+    pub name: String,
+}
+
+#[derive(clap::Parser, Debug)]
+struct Cli {
+    #[arg(short = 'f', long = "fibase_innate")]
+    fibase_innate: Option<PathBuf>,
+    #[arg(short = 's', long = "sibase_innate")]
+    sibase_innate: Option<PathBuf>,
+}
+
+static INNATE: std:sync::OnceLock<Innate> = std::sync::OnceLock::new();
+
+pub(crate) innate()
+{
+    let cli = Cli::parse();
+    println!("cli: {:?}", cli);
+
+    let innate_path = cli.fibase_innate.unwrap_or(cli.sibase_innate.unwrap_or(Path::new("./scenes/dossier/utils/innate.json").to_path_buf()));
+    let innate_file = File::open(innate_path)?;
+    let innate_reader = BufReader::new(innate_file);
+    let innate: Innate = serde_json::from_reader(innate_reader)?;
+    println!("innate {:?}", innate);
+
+	INNATE.set(innate);
+	//let _ = INNATE.set(innate);
+
+}
--- scenes/dossier/src/main.rs
+++ scenes/dossier/src/main.rs
@@ -19,39 +19,6 @@ use opentelemetry::{
 
 // pub use greeter::*;
 
-// 上电初始化一次的参数 'static
-#[derive(Deserialize, Debug)]
-pub struct Innate {
-	// 公共信息存放的位置，如数据库文件assets.db
-    pub fibase_shrine: PathBuf,
-    pub sibase_shrine: PathBuf,
-
-	// 参数存放的位置
-    pub fibase_valver: PathBuf,
-    pub sibase_valver: PathBuf,
-
-    pub fibase_snaper: PathBuf,
-    pub sibase_snaper: PathBuf,
-
-    pub sibase_larder: PathBuf,
-    pub fibase_larder: PathBuf,
-    pub sxmass_larder: PathBuf,
-    pub fxmass_larder: PathBuf,
-    pub sxplug_larder: PathBuf,
-    pub fxplug_larder: PathBuf,
-
-    pub id: u16,
-    pub name: String,
-}
-
-#[derive(clap::Parser, Debug)]
-struct Cli {
-    #[arg(short = 'f', long = "fibase_innate")]
-    fibase_innate: Option<PathBuf>,
-    #[arg(short = 's', long = "sibase_innate")]
-    sibase_innate: Option<PathBuf>,
-}
-
 #[derive(Debug)]
 struct AppState {
 	toolkit: Vec<Toolkit>
@@ -62,25 +29,11 @@ struct AppState {
     http_body_gauge: Histogram<u64>,
 }
 
-static INNATE: std:sync::OnceLock<Innate> = std::sync::OnceLock::new();
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use spiders_dossier::{app::*, fallback::file_and_error_handler};
-
-    let cli = Cli::parse();
-    println!("cli: {:?}", cli);
-
-    let innate_path = cli.fibase_innate.unwrap_or(cli.sibase_innate.unwrap_or(Path::new("./scenes/dossier/utils/innate.json").to_path_buf()));
-    let innate_file = File::open(innate_path)?;
-    let innate_reader = BufReader::new(innate_file);
-    let innate: Innate = serde_json::from_reader(innate_reader)?;
-    println!("innate {:?}", innate);
-
-	INNATE.set(innate);
-	//let _ = INNATE.set(innate);
 
 	toolkit_init().await?
 
@@ -159,148 +112,4 @@ async fn handler(State(state): State<Arc<AppState>>) -> Html<&'static str> {
 
     error!("respond");
     Html("<h1>Hello, World! I am Spiders.</h1>")
-}
-
-async fn toolkit_init() -> Result<(), Box<dyn Error>>
-{
-	use futures_util::future::ready;
-	use futures_util::stream::StreamExt;
-	use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
-
-	// 创建sigrok事件监控任务
-    let builder = MonitorBuilder::new()
-		.expect("Couldn't create builder")
-		.match_subsystem_devtype("usb", "usb_device")
-		.expect("Failed to add filter for USB devices");
-	let monitor: AsyncMonitorSocket = builder
-		.listen()
-		.expect("Couldn't create MonitorSocket")
-		.try_into()
-		.expect("Couldn't create AsyncMonitorSocket");
-	monitor.for_each(toolkit_sigrok_event).await;
-
-	// 创建sigrok定期采集任务
-	tokio::spawn(toolkit_sigrok_glean);
-
-	// 创建serial事件监控任务
-    let builder = MonitorBuilder::new()
-		.expect("Couldn't create builder")
-		.match_subsystem_devtype("usb", "usb_device")
-		.expect("Failed to add filter for USB devices");
-	let monitor: AsyncMonitorSocket = builder
-		.listen()
-		.expect("Couldn't create MonitorSocket")
-		.try_into()
-		.expect("Couldn't create AsyncMonitorSocket");
-	monitor.for_each(toolkit_sigrok_event).await;
-
-	// 创建serial定期采集任务
-	tokio::spawn(toolkit_serial_glean);
-}
-
-async fn toolkit_sigrok_event(event) -> Result<(), Box<dyn Error>>
-{
-	if let Ok(event) = event {
-		println!(
-			"Hotplug event: {}: {}",
-			event.event_type(),
-			event.device().syspath().display()
-		);
-	}
-	ready(())
-}
-
-async fn toolkit_sigrok_glean() -> Result<(), Box<dyn Error>>
-{
-	loop {
-		println!("udev enumerator init");
-		let mut enumerator = udev::Enumerator::new()?;
-		enumerator.match_subsystem("usb")?;
-		enumerator.match_property("ID_SIGROK", "1");
-		 
-		for device in enumerator.scan_devices()? {
-		    // println!("{:#?}", device.syspath());
-		    // println!("{:#?}", device.devpath());
-		    println!();
-		    println!("{:#?}", device);
-		 
-		    println!("  [properties]");
-		    for property in device.properties() {
-		        println!("    - {:?} {:?}", property.name(), property.value());
-		    }
-		 
-		    println!("  [attributes]");
-		    for attribute in device.attributes() {
-		        println!("    - {:?} {:?}", attribute.name(), attribute.value());
-		    }
-		}
-		println!("udev enumerator free");
-	}
-
-    Ok(())
-}
-
-async fn toolkit_serial_event(event) -> Result<(), Box<dyn Error>>
-{
-	if let Ok(event) = event {
-		println!(
-			"Hotplug event: {}: {}",
-			event.event_type(),
-			event.device().syspath().display()
-		);
-	}
-	ready(())
-}
-
-async fn toolkit_serial_glean() -> Result<(), Box<dyn Error>>
-{
-	use serialport::{available_ports, SerialPortType};
-
-    match available_ports() {
-        Ok(ports) => {
-            println!("ports count {}", ports.len());
-            for p in ports {
-                println!("ports name {}", p.port_name);
-                match p.port_type {
-                    SerialPortType::UsbPort(info) => {
-                        println!("Type: USB info {:?}", info);
-                        println!("    VID:{:04x} PID:{:04x}", info.vid, info.pid);
-                        println!(
-                            "     Serial Number: {}",
-                            info.serial_number.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "      Manufacturer: {}",
-                            info.manufacturer.as_ref().map_or("", String::as_str)
-                        );
-                        println!(
-                            "           Product: {}",
-                            info.product.as_ref().map_or("", String::as_str)
-                        );
-                        #[cfg(feature = "usbportinfo-interface")]
-                        println!(
-                            "         Interface: {}",
-                            info.interface
-                                .as_ref()
-                                .map_or("".to_string(), |x| format!("{:02x}", *x))
-                        );
-                    }
-                    SerialPortType::BluetoothPort => {
-                        println!("Type: BluetoothPort");
-                    }
-                    SerialPortType::PciPort => {
-                        println!("Type: PciPort");
-                    }
-                    SerialPortType::Unknown => {
-                        println!("Type: Unknown");
-                    }
-                }
-            }
-        }
-        Err(e) => {
-            eprintln!("{:?}", e);
-        }
-    }
-
-    Ok(())
 }
