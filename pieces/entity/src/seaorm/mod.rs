4ef1fd63eb497681175dfa746fb4530ec8651c25
Cuprum Wangzuo <cuprumwz@foxmail.com>
2025-01-04 08:05:27 +0800

    leptos 0.7

--- pieces/entity/src/seaorm/mod.rs
+++ pieces/entity/src/seaorm/mod.rs
@@ -4,7 +4,12 @@ mod snooper;
 mod toolkit;
 mod utensil;
 
+// use sea_orm::DbConn;
 pub use snooper::*;
 pub use toolkit::*;
 pub use utensil::*;
 
+// // get snooper vec include toolkit include utensil info
+// pub fn find_all_snooper(db: &DbConn) -> Result<Vec<Vec<>>>{
+//     
+// }
