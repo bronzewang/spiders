d4a7287c0f29bd03505067ccda252d9bcaaee7aa
Cuprum Wangzuo <cuprumwz@foxmail.com>
2025-03-12 23:53:50 +0800

    widget

--- pieces/widget/src/lib.rs
+++ pieces/widget/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use leptos::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[component]
+fn Utensil() -> impl IntoView {
+    view! {
+        <p> "test" </p>
     }
 }
