868be6c4d922d9e74059ee78df04002a0d35b8f5
Cuprum Wangzuo <cuprumwz@foxmail.com>
2025-08-23 20:47:39 +0800

    clean spiders-visitor

--- spiders-visitor/src/main.rs
+++ spiders-visitor/src/main.rs
@@ -1,30 +1,3 @@
-// mod app;
-// use app::*;
-
-// mod book;
-// use book::*;
-
-mod node;
-use node::*;
-
-use leptos::*;
 
 fn main() {
-    console_error_panic_hook::set_once();
-    tracing_wasm::set_as_global_default_with_config(
-        tracing_wasm::WASMLayerConfigBuilder::default()
-            .set_max_level(tracing::Level::DEBUG)
-            .build(),
-    );
-
-    // let conn = Database::connect("sqlite::memory:")
-    //     .await
-    //     .expect("Database connection failed");
-
-    mount_to_body(|| {
-        view! {
-            // <BookList />
-            <Node />
-        }
-    })
 }
