cbe61d1cd22fd76fb82dd288342f1c7034af72dd
Cuprum Wangzuo <cuprumwz@foxmail.com>
2025-08-23 21:48:32 +0800

    spiders-visitor clean

--- spiders-visitor/src/main.rs
+++ spiders-visitor/src/main.rs
@@ -1,3 +1,25 @@
+use std::error::Error;
+use std::time::Instant;
+use std::time::Duration;
+use clap::Parser;
 
-fn main() {
+#[derive(Debug, Parser)]
+struct Cli {
+    #[arg(short, long, default_value_t = 100)]
+    rate: u64,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    println!("test");
+    let rate = Duration::from_millis(cli.rate);
+    run(rate)
+}
+
+fn run(_rate: Duration) -> Result<(), Box<dyn Error>> {
+    let mut _last_tick = Instant::now();
+    // loop {
+    //     println!("test");
+    // }
+    Ok(())
 }
