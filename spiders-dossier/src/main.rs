c88dde9edd9fd48922cbaf42315c906f8a74a57e
Cuprum Wangzuo <cuprumwz@foxmail.com>
2025-08-25 00:29:42 +0800

    compile done

--- spiders-dossier/src/main.rs
+++ spiders-dossier/src/main.rs
@@ -1,25 +1,28 @@
 use clap::Parser;
 use dora_node_api::{DoraNode, Event};
-use opentelemetry_appender_log::OpenTelemetryLogBridge;
-use opentelemetry_sdk::{logs::SdkLoggerProvider, metrics::SdkMeterProvider, Resource};
+// use opentelemetry_appender_log::OpenTelemetryLogBridge;
+// use opentelemetry_sdk::{logs::SdkLoggerProvider, metrics::SdkMeterProvider, Resource};
 use prometheus::{Registry};
-use opentelemetry::{
-    metrics::{Counter, Histogram, MeterProvider as _, Unit},
-    KeyValue,
-};
+// use opentelemetry::{
+//     metrics::{Counter, Histogram, MeterProvider as _, Unit},
+//     KeyValue,
+// };
 use tonic::transport::Server;
 use std::sync::Arc;
 use std::error::Error;
 use std::net::SocketAddr;
 use log::{error, Level};
 
-use spiders_dossier::{app::*};
+// use spiders_dossier::{app::*};
 
 pub mod greeter {
 	include!(concat!(env!("OUT_DIR"), "/greeter.rs"));
 }
 
 pub use greeter::*;
+
+use crate::greeter_server::Greeter;
+use crate::greeter_server::GreeterServer;
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -33,8 +36,8 @@ struct AppState {
 	// snooper: Vec<Snooper>,
 
     registry: Registry,
-    tonic_counter: Counter<u64>,
-    tonic_body_gauge: Histogram<u64>,
+    // tonic_counter: Counter<u64>,
+    // tonic_body_gauge: Histogram<u64>,
 }
 
 #[tokio::main]
@@ -44,52 +47,52 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 	// toolkit_init().await?;
 
-    let exporter = opentelemetry_stdout::LogExporter::default();
-    let logger_provider = SdkLoggerProvider::builder()
-        .with_resource(
-            Resource::builder()
-                .with_service_name("log-appender-tracing-example")
-                .build(),
-        )
-        .with_simple_exporter(exporter)
-        .build();
+    // let exporter = opentelemetry_stdout::LogExporter::default();
+    // let logger_provider = SdkLoggerProvider::builder()
+    //     .with_resource(
+    //         Resource::builder()
+    //             .with_service_name("log-appender-tracing-example")
+    //             .build(),
+    //     )
+    //     .with_simple_exporter(exporter)
+    //     .build();
 
-    // Setup Log Appender for the log crate.
-    let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
-    log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
-    log::set_max_level(Level::Error.to_level_filter());
+    // // Setup Log Appender for the log crate.
+    // let otel_log_appender = OpenTelemetryLogBridge::new(&logger_provider);
+    // log::set_boxed_logger(Box::new(otel_log_appender)).unwrap();
+    // log::set_max_level(Level::Error.to_level_filter());
 
     let registry = Registry::new();
-    let exporter = opentelemetry_prometheus::exporter()
-        .with_registry(registry.clone())
-        .build()?;
-    let provider = SdkMeterProvider::builder().with_reader(exporter).build();
+    // let exporter = opentelemetry_prometheus::exporter()
+    //     .with_registry(registry.clone())
+    //     .build()?;
+    // let provider = SdkMeterProvider::builder().with_reader(exporter).build();
 
-    let meter = provider.meter("spiders-dossier");
+    // let meter = provider.meter("spiders-dossier");
     let state = Arc::new(AppState {
 		// toolkit: Vec::new(),
 		// snooper: Vec::new(),
         registry: registry,
-        tonic_counter: meter
-            .u64_counter("tonic_requests_total")
-            .with_description("Total number of TONIC requests made.")
-            .init(),
-        tonic_body_gauge: meter
-            .u64_histogram("example.tonic_response_size")
-            .with_unit(Unit::new("By"))
-            .with_description("The metrics TONIC response sizes in bytes.")
-            .init(),
+        // tonic_counter: meter
+        //     .u64_counter("tonic_requests_total")
+        //     .with_description("Total number of TONIC requests made.")
+        //     .init(),
+        // tonic_body_gauge: meter
+        //     .u64_histogram("example.tonic_response_size")
+        //     .with_unit(Unit::new("By"))
+        //     .with_description("The metrics TONIC response sizes in bytes.")
+        //     .init(),
     });
 
     let addr = cli.addr.unwrap_or_else(|| "[::1]:29000".parse().unwrap());
 	// addr.set_port(addr.port()+innate.id);
 
     tokio::select! {
-        dora_result = dora_server(state) => {
-            println!("dora task completed with result: {}", dora_result);
+        dora_result = dora_server(state.clone()) => {
+            // println!("dora task completed with result: {}", dora_result);
         },
         tonic_result = tonic_server(state, addr) => {
-            println!("tonic task completed with result: {}", tonic_result);
+            // println!("tonic task completed with result: {}", tonic_result);
         },
     }
 
@@ -98,7 +101,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn dora_server(state: Arc<AppState>) -> Result<(), Error> {
+async fn dora_server(state: Arc<AppState>) -> Result<(), Box<dyn Error>> {
     let (_node, mut events) = DoraNode::init_from_env()?;
     // let (_node, mut events) =
     //     DoraNode::init_from_node_id(NodeId::from("spiders-dossier-dyn".to_string()))?;
@@ -133,14 +136,54 @@ async fn dora_server(state: Arc<AppState>) -> Result<(), Error> {
             other => eprintln!("Received unexpected input: {other:?}"),
         }
     }
-    Ok(());
+
+    Ok(())
 }
 
-async fn tonic_server(state: Arc<AppState>, addr: SocketAddr) -> Result<(), Error> {
+#[derive(Default)]
+struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    /// 登签
+    async fn sign(
+        &self,
+        request: tonic::Request<SignPing>,
+    ) -> std::result::Result<tonic::Response<SignPong>, tonic::Status> {
+        Ok(tonic::Response::new(SignPong { timestamp: 0 }))
+    }
+
+    /// 链路维持
+    async fn keep(
+        &self,
+        request: tonic::Request<KeepPing>,
+    ) -> std::result::Result<tonic::Response<KeepPong>, tonic::Status> {
+        Ok(tonic::Response::new(KeepPong { timestamp: 0 }))
+    }
+
+    /// Toolkit列表
+    async fn toolkit_list(
+        &self,
+        request: tonic::Request<ToolkitListPing>,
+    ) -> std::result::Result<tonic::Response<ToolkitListPong>, tonic::Status> {
+        Ok(tonic::Response::new(ToolkitListPong { toolkits: Vec::new() }))
+    }
+
+    /// Snooper列表
+    async fn snooper_list(
+        &self,
+        request: tonic::Request<SnooperListPing>,
+    ) -> std::result::Result<tonic::Response<SnooperListPong>, tonic::Status> {
+        Ok(tonic::Response::new(SnooperListPong { snoopers: Vec::new() }))
+    }
+}
+
+async fn tonic_server(state: Arc<AppState>, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
     error!("listening on {}", addr);
+    let greeter = MyGreeter::default();
     Server::builder()
         .add_service(GreeterServer::new(greeter))
         .serve(addr)
         .await?;
-    Ok(());
+    Ok(())
 }
