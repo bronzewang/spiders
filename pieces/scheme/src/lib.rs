963cdfbb9f0572566b1395f04b5debfa5f7f0dfc
Bronze Wang <cuprumwz@foxmail.com>
2025-09-01 23:38:05 +0800

    scheme

--- pieces/scheme/src/lib.rs
+++ pieces/scheme/src/lib.rs
@@ -1,6 +1,6 @@
-pub const SPIDERS_WEB_PORT_BASE: u16 = 63000;
-pub const SPIDERS_GRPC_PORT_BASE: u16 = 22000;
-pub const SPIDERS_WEBSOCKET_PORT_BASE: u16 = 55000;
+// pub const SPIDERS_WEB_PORT_BASE: u16 = 63000;
+pub const SPIDERS_GRPC_PORT_BASE: u16 = 29000;
+// pub const SPIDERS_WEBSOCKET_PORT_BASE: u16 = 55000;
 
 pub mod server {
 	tonic::include_proto!("server");
