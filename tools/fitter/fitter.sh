e67c8d79dd0a07f0e68b7eed9f3c0a920416b958
Bronze Wang <cuprumwz@foxmial.com>
2026-01-03 02:23:48 +0800

    fitter

--- tools/fitter/fitter.sh
+++ tools/fitter/fitter.sh
@@ -47,6 +47,10 @@ sudo apt install translate-shell
 ## 配置浏览器的默认网址
 ## lazygit
 ##//sudo apt install lazygit   //ubuntu25.10
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 ## gitu
 cargo binstall gitu
 ## shellharden
@@ -55,6 +59,7 @@ cargo binstall shellharden
 cargo binstall aichat
 ## iamb
 cargo binstall iamb
+## remmina的默认配置
 
 # 桥接机
 ## 加快网络访问设置，隧道和镜像
